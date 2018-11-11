a54243f89a6c58c0187da5ed81a0ccff069450f3
root <gordon.meurer@gmail.com>
2018-11-11 13:31:49 +0000

    removed Vundle.vim and adjusted init script to clone vundle repo

--- init.sh
+++ init.sh
@@ -1,3 +1,10 @@
+
+echo "Linking vim directory and vimrc file to the default path"
 ln -s $PWD/.vim/  ~/.vim
 ln $PWD/.vimrc  ~/.vimrc
 echo "Everything is now linked to the default path"
+
+
+echo "Installing 'Vundle' for package managing..."
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "Vundle installed!"
